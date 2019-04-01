9f915e7e82cc43e54aa799ba2911de9d168d6ce2
Ahileshanbu <ahileshanbalagan@gmail.com>
2019-04-01 11:00:10 +0530

    Create Basics.py

---
+++ Basics.py
@@ -0,0 +1,11 @@
+num1 = int (input ("Enter the num1 "))
+num2 = int (input ("Enter the num2 "))
+
+print("The sum of two number is ",num1+num2)
+print("The difference between two number is ",abs(num1-num2))
+
+print("The multiplication of two number is ",num1*num2)
+
+print("The division of two number is ", num1/num2)
+print("The quotient of two number is ", num1//num2)
+print("The remainder of two number is ", num1%num2)
