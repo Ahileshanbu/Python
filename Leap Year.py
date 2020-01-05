6875ba7471c5c3e3796dfb50aaaf4a4874503301
Ahileshanbu <ahileshanbalagan@gmail.com>
2020-01-05 14:35:15 +0530

    Update Leap Year.py

--- Leap Year.py
+++ Leap Year.py
@@ -1,3 +1,5 @@
+#Leap year or not
+
 num = int(input("Enter the Year: "))
 
 if num%4==0 :
@@ -10,3 +12,32 @@ if num%4==0 :
         print("Given year is leap year")
 else:
     print("Given year is not a leap year")
+
+
+
+#Positive or not
+
+num = int(input("Enter the number: "))
+
+if num>0:
+    print("Given number is Positive")
+elif num<0:
+    print("Given number is Negative")
+else:
+    print("Given number is Zero")
+
+
+
+
+#Largest number
+
+num1 = int(input("Enter the num1: "))
+num2 = int(input("Enter the num2: "))
+num3 = int(input("Enter the num3: "))
+
+if num1>num2 and num1>num3 :
+    print(num1, "is greater")
+elif num2>num1 and num2>num3 :
+    print(num2, "is greater")
+else:
+    print(num3, "is greater")
